0f3b180ac21e7fa03f915a2571bf203cfca5c5e5
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2025-05-02 19:50:35 -0300

    atualizações

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Landing_Page_SENAI/',
+  base: '/Landin_Page_SENAI/',
   plugins: [react()],
 })

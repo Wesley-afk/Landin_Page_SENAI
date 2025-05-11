9db2419a8edefd7377760f5bae3448d0a077873e
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2025-05-11 00:10:56 -0300

    correções a navbar com mais responsivdade

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,129 +3,94 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
 import Login from './Login/Login';
+import Home from '../pages/Home';
 import styles from './Login/styles/login.module.css';
-import stales from './src/components/Nav.module.css'
 import { useNavigate } from 'react-router-dom';
-import Home from "../pages/Home"
 
 function BasicExample() {
     const navigate = useNavigate();
     const [mostrarLogin, setMostrarLogin] = useState(false);
-    const [home, setHome] = useState(false);
-
+    const [mostarHome, setMostrarHome] = useState(false);
     return (
         <>
-            <Navbar expand="lg" className="bg-body-primary" fixed='top' 
-                    style={{
-                         backgroundColor: '#0746B4',
-                         display: 'flex',
-                         alignItems: 'center',
-                         justifyContent: 'center',
-                         maxHeight: '11%',
-                         overflow: 'hidden',
-                    }}>
+            <Navbar expand="lg" className="bg-body-primary" fixed='top' style={{ backgroundColor: '#0746B4', display: 'flex', alignItems: 'center', height: '11vh' }}>
                 <Container>
-                    {/* Ajustar o estilo da navbar */}
-                      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-                      {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-                        {/* imagem da logo do senai */}
-                        <Navbar.Brand href="#home" style={{padding: '0', maxWidth: '150px', width: '100%'}} onClick={() => setHome(!home)}>
-                            <img
-                                src="./LogoSenai.svg"
-                                alt="LogoSenai" 
-                                style={{
-                                         width: '100%',
-                                         height: 'auto',
-                                         display: 'block',
-                                         cursor: 'pointer'
-                                }}     
-                            />
-                        </Navbar.Brand>
+                    <Navbar.Brand href="#home">
+                        <img src="./LogoSenai.svg" alt="LogoSenai" 
+                        onClick={() => navigate('/')}
+                        />
+                    </Navbar.Brand>
+                    <NavDropdown color='#FFFFFF' title="Menu" id="basic-nav-dropdown" 
+                        style={{
+                            color: '#FFFFFF',
+                            fontSize: '15px',
+                        }}>
+                        <div style={{
+                            //   Se der tempo fazer igual
+                        }}>
+                            <NavDropdown.Item href="#action/3.1" onClick={() => navigate('/Historia')}>Nossa história!</NavDropdown.Item>
+                            <NavDropdown.Item href='#action/3.2'></NavDropdown.Item>
+                            <NavDropdown.Divider />
+                        </div>
+                    </NavDropdown>
 
-                        {/* Parte que leva para a história <> */}
-                        <NavDropdown color='#FFFFFF' title="Menu" id="basic-nav-dropdown"
-                            style={{
-                                position:'relative',
-                                color: 'white',
-                            }}>
-                            {/* Parte que leva para a história </> */}
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">
 
-                            <div style={{overflow: 'visible', position:'relative'}}>
-                                <NavDropdown.Item href="#action/3.1" onClick={() => navigate('/Historia')} style={{overflow:'visible', position:'relative'}}>Nossa história!</NavDropdown.Item>
-                                <NavDropdown.Item href='#action/3.2'></NavDropdown.Item>
-                                <NavDropdown.Divider />
-                            </div>
-                        </NavDropdown>
-
-                        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-                        {/* Desativar o navbar collapse depois */}
-                        {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-                        <Nav className="me-auto d-flex align-items-center">
-                            <Nav.Link href="#home" style={{padding:'0'}}>
-
-                                {/* Área de pesquisa */}
-                                <div className="form-floating mb-3">
-                                    <input
-                                        type="text"
+                                <div className="form-floating mb-3" style={{ width: 900, display: 'flex', alignItems: 'center' }}>
+                                    <input type="text"
                                         className="form-control"
                                         id="floatingInput"
                                         style={{
-                                            position:'relative',
-                                            left:'28%',
-                                            overflow:'visible'
+                                            position: 'relative',
+                                            top: '3vh',
+                                            height: '4vh',
+                                            width: '50vw',
+                                            padding: '1%',
+                                            backgroundColor: 'white',
                                         }}
                                     />
                                 </div>
                                 <label htmlFor="floatingInput"
                                     style={{
-                                        // display: 'inline-flex',
-                                        position: 'absolute',
-                                        left:'24%',
-                                        bottom: '2vh',
-                                        fontSize: 'clamp(2px, 2.3vw, 15px)'
-                                    }}>
-                                        Pesquise o nome do curso <img src="lupa.svg" alt="Buscar" />
+                                        position: 'relative',
+                                        bottom: '5vh',
+                                        left: '1vw'
+                                    }}>Pesquise o nome do curso <img src="lupa.svg" alt="Buscar" />
                                 </label>
                             </Nav.Link>
                             <img src="/user.svg" alt="Icone do usuário" onClick={() => setMostrarLogin(!mostrarLogin)} style={{
-                                position: 'absolute',
-                                right: '29%',
-                                top: '32%',
-                                height: 'auto',
-                                cursor: 'pointer'
-                                // Mexer um pouco aqui ainda
+                                position: 'relative',
+                                right: '6vw'
                             }} />
                             <p style={{
+                                position: 'relative',
                                 color: 'white',
-                                position: 'absolute',
-                                right: '20%',
-                                width: '8%',
-                                height: 'auto',
-                                fontSize: 'clamp(20px, auto, 2vh)',
-                                top: '20%',
+                                top: '5vh',
+                                right: '5.8vw',
+                                width: '9vw',
                             }}>
-                                Logue ou cadastre-se
+                                Olá, faça login ou cadastre-se
                             </p>
 
                             <Nav.Link href="#home">
                                 <label htmlFor="floatingInput">
                                     <img src="/FavoritarCurso.svg" style={{
-                                        position: 'absolute',
-                                        top: '34%',
-                                        right: '18%',
-                                        height: 'auto'
+                                        paddingTop: '5vh',
+                                        position: 'relative',
+                                        right: '4vw'
                                     }} />
                                 </label>
                             </Nav.Link>
                         </Nav>
-                        {home && <Home/>}
                         {mostrarLogin && <div className={styles.LayOutLogin}><Login /></div>}
-                        {/* </Navbar.Collapse> */}
+                        {mostarHome && <Home />}
+                    </Navbar.Collapse>
                 </Container>
-            </Navbar>
+            </Navbar >
         </>
     );
 }
